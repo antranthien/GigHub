d9e5d84117373237e9b7ab95e91a806474eeef9a
An <an.t.thien@hiof.no>
2016-05-29 21:25:19 +0200

    Implement Saving a gig

--- GigHub/Models/Genre.cs
+++ GigHub/Models/Genre.cs
@@ -4,7 +4,7 @@ namespace GigHub.Models
 {
     public class Genre
     {
-        public int Id { get; set; }
+        public byte Id { get; set; }
 
         [Required]
         [StringLength(255)]
