193f6e59c20a04ccef946c4812a190142856a7a8
An <an.t.thien@hiof.no>
2016-06-01 17:26:48 +0200

    Add Attendance model

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -8,6 +8,9 @@ namespace GigHub.Models
         public DbSet<Gig> Gigs { get; set; }
 
         public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Attendance> Attendances { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -16,6 +19,16 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig) // Each Attendance has a required Gig
+                .WithMany() // The reversed direction, each Gig has many Attendances
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
